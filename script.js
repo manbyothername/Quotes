91f91c768415b7a640ca63ea13877d69afaf30fa
Osvaldo Rodriguez <osvaldo.gabrielrc@outlook.com>
2021-07-25 11:14:08 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,30 +3,46 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn= document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
 
 let apiQuotes = [];
 
+// Show laoding
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+
+}
+
+// Hide loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden =true;
+}
 //New Quote
 function newQuote(){
+    complete();
     //Random quote
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
     
     quoteText.textContent = quote.text;
 }
-//Author field
-if (!quote.Author) {
-    authorText.textContent = 'unknown';
-} else {
-        authorText.textContent = authorText;
-    }
+
 // Check quote length
 if(quoteText.length > 50) {
     quoteText.classList.add('long-quote');
 } else {
     quoteText.classList.remove('long-quote');
 }
+// Set quote
+quoteText.textContent = quoteText;
+complete();
+//Author field
+authorText.textContent = quote.author ? quote.author : "Unknown";
 //Get Quotes fom API
 async function getQuotes(){
+    loading();
     const apiUrl = 'https://type.fit/api/quotes';
     try {
         const response = await fetch(apiUrl);
